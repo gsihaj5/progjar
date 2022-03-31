e61c94e63fc389de2408114efb262be05b907509
gerry sihaj <gsihaj5@gmail.com>
2022-03-31 22:59:09 +0700

    create super simple client program

--- ETS/client.py
+++ ETS/client.py
@@ -1,1 +1,29 @@
-# test
+import socket
+import logging
+
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the port where the server is listening
+    server_address = ('172.16.16.101', 10000)
+    print(f"connecting to {server_address}")
+    sock.connect(server_address)
+
+    # Send data
+    message = 'INI ADALAH DATA YANG DIKIRIM ABCDEFGHIJKLMNOPQ'
+    print(f"sending {message}")
+    sock.sendall(message.encode())
+
+    amount_received = 0
+    amount_expected = len(message)
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print(f"{data}")
+        logging.info(f"{data}")
+except Exception as ee:
+    logging.info(f"ERROR: {str(ee)}")
+    exit(0)
+finally:
+    logging.info("closing")
+    sock.close()
