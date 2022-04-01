f46bba076435a89176f6f94d237c383b1eaaaf78
gerry sihaj <gsihaj5@gmail.com>
2022-04-01 09:56:58 +0700

    add secure connection from client

--- ETS/client.py
+++ ETS/client.py
@@ -1,22 +1,47 @@
 import socket
 import logging
 import threading
+import ssl
+import os
 
 
 class Request:
-    def __init__(self, ip, port, data):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self, ip, port, data, secure=False):
         self.ip = ip
         self.port = port
         self.data = data
+        self.secure = secure
 
-    def connect(self):
+    def make_socket(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_address = (self.ip, self.port)
-        print(f"connecting to {server_address}")
+        logging.warning(f"connecting to {server_address}")
         self.sock.connect(server_address)
 
+    def make_secure_socket(self):
+        try:
+            # get it from https://curl.se/docs/caextract.html
+            context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+            context.verify_mode = ssl.CERT_OPTIONAL
+            context.load_verify_locations(os.getcwd() + '/domain.crt')
+
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_address = (self.ip, self.port)
+            logging.warning(f"connecting to {server_address}")
+            self.sock.connect(server_address)
+            secure_socket = context.wrap_socket(
+                self.sock, server_hostname=self.ip)
+            logging.warning(secure_socket.getpeercert())
+            self.sock = secure_socket
+
+        except Exception as ee:
+            logging.warning(f"error {str(ee)}")
+
     def send(self):
-        self.connect()
+        if(self.secure):
+            self.make_socket()
+        else:
+            self.make_secure_socket()
         print(f"sending {self.data}")
         self.sock.sendall(self.data.encode())
         self.handle_response()
@@ -39,19 +64,26 @@ class Request:
 
 
 try:
-    request1 = Request('172.16.16.101', 10000, "hello1111111111111111")
-    request2 = Request('172.16.16.101', 10000, "222")
-    request3 = Request('172.16.16.101', 10000,
-                       "33333333333333333333333333333333")
+    request1 = Request(
+        ip='172.16.16.101',
+        port=10000,
+        data="getdatapemain 1\r\n\r\n",
+        secure=True
+    )
+    request2 = Request(
+        ip='172.16.16.101',
+        port=10000,
+        data="getdatapemain 2\r\n\r\n",
+        secure=True
+    )
+    request3 = Request(
+        ip='172.16.16.101',
+        port=10000,
+        data="getdatapemain 3\r\n\r\n",
+        secure=True
+    )
 
     request1.send_with_thread()
-    request2.send_with_thread()
-    request3.send_with_thread()
-
-    request1.join_thread()
-    request2.join_thread()
-    request3.join_thread()
-
     print("done")
 
 except Exception as ee:
